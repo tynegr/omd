57a0b1dcc5f14fcaa14a56e4aa8a5085fc17214e
tynegr <122130536+tynegr@users.noreply.github.com>
2023-11-06 17:11:13 +0300

    Add files via upload

---
+++ issue_2.py
@@ -0,0 +1,53 @@
+import pytest
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def decode(morse_message: str) -> str:
+    '''
+    Декодирует строку из азбуки Морзе в английский
+
+    '''
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+@pytest.mark.parametrize("morse, expected", [
+    ('-- .---   .. ...   - .... .   --. --- .- -',
+     'MJISTHEGOAT'),
+    ('.-. ..- ... ... . .-.. .-- . ... - -... .-. .. -.-. -.-', 'RUSSELWESTBRICK'),
+    ('.- ...- .. - --- .. ... --. --- --- -..', 'AVITOISGOOD'),
+    ('.--. -.-- - .... --- -.', 'PYTHON'),
+    ('-.-. .--. .-.. ..- ... .--. .-.. ..- ...', 'CPLUSPLUS'),
+])
+
+def test_decode(morse, expected):
+    decoded = decode(morse)
+    assert decoded == expected
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ issue_4.py
@@ -0,0 +1,80 @@
+from typing import List, Tuple
+import pytest
+
+
+def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
+    '''
+    fit_transform(iterable)
+    fit_transform(arg1, arg2, *args)
+    '''
+    if len(args) == 0:
+        raise TypeError('expected at least 1 arguments, got 0')
+
+    categories = args if isinstance(args[0], str) else list(args[0])
+    uniq_categories = set(categories)
+    bin_format = f'{{0:0{len(uniq_categories)}b}}'
+
+    seen_categories = dict()
+    transformed_rows = []
+
+    for cat in categories:
+        bin_view_cat = (int(b) for b in
+                        bin_format.format(1 << len(seen_categories)))
+        seen_categories.setdefault(cat, list(bin_view_cat))
+        transformed_rows.append((cat, seen_categories[cat]))
+
+    return transformed_rows
+
+
+def test_fit_transform_one_word():
+    '''
+        Тестируем, что функция корректно кодирует одну строку
+    '''
+    input_data = 'nuggets'
+    expected_result = [('nuggets', [1])]
+    result = fit_transform(input_data)
+    assert result == expected_result
+
+def test_fit_transform_list_of_words():
+    '''
+        Тестируем, что функция корректно кодирует список строк
+    '''
+    input_data = ['clippers', 'nicks', 'lakers', 'bulls']
+    expected_result = [
+        ('clippers', [0, 0, 0, 1]),
+        ('nicks', [0, 0, 1, 0]),
+        ('lakers', [0, 1, 0, 0]),
+        ('bulls', [1, 0, 0, 0])
+    ]
+    result = fit_transform(*input_data)
+    assert result == expected_result
+
+def test_fit_transform_missed_word():
+    '''
+        Тестируем, что функция правильно обрабатывает пустые строки
+    '''
+    input_data = ['lebron', '', 'james']
+    expected_result = [('lebron', [0, 0, 1]), ('', [0, 1, 0]),
+                       ('james', [1, 0, 0])]
+    result = fit_transform(*input_data)
+    assert result == expected_result
+
+def test_fit_transform_no_arguments():
+    '''
+        Тестируем, что функция вызывает исключение при отсутствии аргументов
+
+    '''
+    with pytest.raises(TypeError):
+        fit_transform()
+
+def test_fit_transform_assertNotIn():
+    '''
+        Тестируем, что функция правильно обрабатывает отсутствующие категории
+    '''
+    input_data = ['curry', 'durant', 'irving']
+    result = fit_transform(*input_data)
+    for category, _ in result:
+        assert category not in ['kareem', 'iverson']
+
+if __name__ == '__main__':
+    pytest.main()
