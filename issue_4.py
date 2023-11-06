3cf430066e454066e782ebc08fe3ca89822e31e7
tynegr <122130536+tynegr@users.noreply.github.com>
2023-11-06 17:42:10 +0300

    Update issue_4.py

--- issue_4.py
+++ issue_4.py
@@ -3,10 +3,10 @@ import pytest
 
 
 def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
-    '''
+    """
     fit_transform(iterable)
     fit_transform(arg1, arg2, *args)
-    '''
+    """
     if len(args) == 0:
         raise TypeError('expected at least 1 arguments, got 0')
 
@@ -27,18 +27,18 @@ def fit_transform(*args: str) -> List[Tuple[str, List[int]]]:
 
 
 def test_fit_transform_one_word():
-    '''
+    """
         Тестируем, что функция корректно кодирует одну строку
-    '''
+    """
     input_data = 'nuggets'
     expected_result = [('nuggets', [1])]
     result = fit_transform(input_data)
     assert result == expected_result
 
 def test_fit_transform_list_of_words():
-    '''
+    """
         Тестируем, что функция корректно кодирует список строк
-    '''
+    """
     input_data = ['clippers', 'nicks', 'lakers', 'bulls']
     expected_result = [
         ('clippers', [0, 0, 0, 1]),
@@ -50,9 +50,9 @@ def test_fit_transform_list_of_words():
     assert result == expected_result
 
 def test_fit_transform_missed_word():
-    '''
+    """
         Тестируем, что функция правильно обрабатывает пустые строки
-    '''
+    """
     input_data = ['lebron', '', 'james']
     expected_result = [('lebron', [0, 0, 1]), ('', [0, 1, 0]),
                        ('james', [1, 0, 0])]
@@ -60,17 +60,17 @@ def test_fit_transform_missed_word():
     assert result == expected_result
 
 def test_fit_transform_no_arguments():
-    '''
+    """
         Тестируем, что функция вызывает исключение при отсутствии аргументов
 
-    '''
+    """
     with pytest.raises(TypeError):
         fit_transform()
 
 def test_fit_transform_assertNotIn():
-    '''
+    """
         Тестируем, что функция правильно обрабатывает отсутствующие категории
-    '''
+    """
     input_data = ['curry', 'durant', 'irving']
     result = fit_transform(*input_data)
     for category, _ in result:
