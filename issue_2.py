35758dae10f9ca31124e9ca45a47790c0ad1b192
tynegr <122130536+tynegr@users.noreply.github.com>
2023-11-06 17:40:09 +0300

    Update issue_2.py

--- issue_2.py
+++ issue_2.py
@@ -25,10 +25,10 @@ MORSE_TO_LETTER = {
 
 
 def decode(morse_message: str) -> str:
-    '''
+    """
     Декодирует строку из азбуки Морзе в английский
 
-    '''
+    """
     decoded_letters = [
         MORSE_TO_LETTER[letter] for letter in morse_message.split()
     ]
